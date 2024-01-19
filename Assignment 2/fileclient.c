df64b7cc6ae16ad02c1074268ca1cd4d8dd2dbea
TheMockingJay1013 <navaneeth1013@gmail.com>
2024-01-19 12:07:44 +0530

    ASG2DONE

--- Assignment 2/fileclient.c
+++ Assignment 2/fileclient.c
@@ -36,22 +36,7 @@ int main()
     int k=2;
     char filename[1024] = "trial.txt";
 
-    int f;
-
-    
-    while(1)
-    {
-        printf("Enter the filename : ");
-        scanf("%s",filename);
-        f = open(filename,O_RDONLY);
-        if(f!=-1) break;
-    }
-    printf("FILE FOUND!!\n");
-
-    // input k
-    printf("Enter k : ");
-    scanf("%d",&k);
-
+    // connecting to the server
     // make the connection 
     if((connect(clisock,(struct sockaddr *)&serv_addr,sizeof(serv_addr)))<0)
     {
@@ -59,56 +44,82 @@ int main()
         exit(0);
     }
 
-    // first send the key value to the server
-    buf[0] = '0'+k;
-    // buf[1] = '\0';
-    send(clisock,buf,1,0);
-
-    // start reading from file and sending it 
-    int n = -1;
-
-    while(n!=0)
-    {
-        n = read(f,buf,100);
-        send(clisock,buf,n,0);
-        
-    }
-    buf[0]='#';
-    send(clisock,buf,1,0);
-    printf("Sent file from client to server\n");
-    close(f); // closing the file 
-
-    // client receives the encrypted file from the server ans stores in new file
-
-    f = open("encrypted.txt.enc",O_RDWR|O_CREAT);
-    if(f<0)
-    {
-        perror("Unable to create file");
-        exit(0);
-    }
-
-    int end = 0;
-    int sz;
+    int f;
     while(1)
     {
-        int len  = recv(clisock,buf,100,0);
-        for(int i=0;i<len;i++)
+        while(1)
         {
-            if(buf[i]=='#')
-            {
-                end = 1 ;
-                len = i;
-                break;
-            }
+            printf("Enter the filename : ");
+            scanf("%s",filename);
+            f = open(filename,O_RDONLY);
+            if(f!=-1) break;
         }
-        sz = write(f,buf,len);
-        if(end)break;
-        memset(buf,'\0',sizeof(buf));
-    }
-    write(f,"\0",1);
+        printf("FILE FOUND!!\n");
 
-    printf("File received and stored in encrypted.txt.enc\n");
-    close(f);
+        // input k
+        printf("Enter k : ");
+        scanf("%d",&k);
+
+
+
+        // first send the key value to the server
+        buf[0] = '0'+k;
+        // buf[1] = '\0';
+        send(clisock,buf,1,0);
+
+        // start reading from file and sending it 
+        int n = -1;
+
+        while(n!=0)
+        {
+            n = read(f,buf,100);
+            send(clisock,buf,n,0);
+            
+        }
+        buf[0]='#';
+        send(clisock,buf,1,0);
+        printf("Sent file from client to server\n");
+        close(f); // closing the file 
+
+        // client receives the encrypted file from the server ans stores in new file
+
+        char encfilename[1024];
+        strcpy(encfilename,filename);
+        strcat(encfilename,".enc");
+        f = open(encfilename,O_RDWR|O_CREAT);
+        if(f<0)
+        {
+            perror("Unable to create file");
+            exit(0);
+        }
+
+        int end = 0;
+        int sz;
+        while(1)
+        {
+            int len  = recv(clisock,buf,100,0);
+            for(int i=0;i<len;i++)
+            {
+                if(buf[i]=='#')
+                {
+                    end = 1 ;
+                    len = i;
+                    break;
+                }
+            }
+            sz = write(f,buf,len);
+            if(end)break;
+            memset(buf,'\0',sizeof(buf));
+        }
+        write(f,"\0",1);
+
+        printf("File received and stored in encrypted.txt.enc\n");
+        close(f);
+        printf("Do you want to continue (y/n) : ");
+        char ch;
+        scanf(" %c",&ch);
+        if(ch=='n')break;
+    }
     close(clisock);
 
 
--- Assignment 2/fileserver.c
+++ Assignment 2/fileserver.c
@@ -45,7 +45,7 @@ int main()
 
     while(1)
     {
-        printf("waiting .. \n");
+        
 
         clilen = sizeof(cliaddr);
         newsock = accept(servsock,(struct sockaddr *)&cliaddr,&clilen);
@@ -65,92 +65,100 @@ int main()
             close(servsock);
             int k ;
 
-            int len = -1;
-            len = recv(newsock,buf,1,0);
-            k = atoi(buf);
-            int i=0;
-
-            char filename[1024];
-            sprintf(filename,"%s.%d.txt",inet_ntoa(cliaddr.sin_addr),ntohs(cliaddr.sin_port));
-
-            int fd = open("foo.txt", O_RDWR|O_CREAT);
-            if(fd<0)
-            {
-                printf("unable to open foo.txt\n");
-            }
-            int sz;
-            long long length=0;
-            int end = 0 ;
+            
             while(1)
-            {
-                len = recv(newsock,buf,100,0);
-                for(int i=0;i<len;i++)
+            {    
+                printf("waiting .. \n");
+                int len = -1;
+                len = recv(newsock,buf,1,0);
+                k = atoi(buf);
+                int i=0;
+
+                char filename[1024];
+                sprintf(filename,"%s.%d.txt",inet_ntoa(cliaddr.sin_addr),ntohs(cliaddr.sin_port));
+
+                int fd = open(filename, O_RDWR|O_CREAT|O_TRUNC,0666);
+                if(fd<0)
                 {
-                    if(buf[i]=='#')
-                    {
-                        end = 1;
-                        len = i+1;
-                        break;
-                    }
+                    printf("unable to open foo.txt\n");
                 }
-                sz = write(fd,buf,len);
-                if(end)break;
-                memset(buf,'$',sizeof(buf));
-            }
-            write(fd,"\0",1);
-
-            printf("File contents received and stored in foo.txt\n");
-            close(fd);
-
-            // now reading from the file ,ecrypting it and storing it enc.txt
-            fd = open("foo.txt",O_RDWR);
-
-            int f2 = open("foo.txt.enc",O_RDWR|O_CREAT);
-            if(f2<0)
-            {
-                printf("unable to open enc.txt\n");
-            }
-
-
-            int n = -1;
-
-            end = 0;
-            while(1)
-            {
-                n = read(fd,buf,100);
-                if(n==0)break;
-                for(int i=0;i<n;i++)
+                int sz;
+                long long length=0;
+                int end = 0 ;
+                while(1)
                 {
-                    if(buf[i]=='#')
+                    len = recv(newsock,buf,100,0);
+                    for(int i=0;i<len;i++)
                     {
-                        end = 1;
+                        if(buf[i]=='#')
+                        {
+                            end = 1;
+                            len = i+1;
+                            break;
+                        }
                     }
-                    if(buf[i]>='a' && buf[i]<='z')
-                    {
-                        buf[i] = (buf[i]-'a'+k)%26 + 'a';
-                    }
-                    else if(buf[i]>='A' && buf[i]<='Z')
-                    {
-                        buf[i] = (buf[i]-'A'+k)%26 + 'A';
-                    }
+                    sz = write(fd,buf,len);
+                    if(end)break;
+                    memset(buf,'$',sizeof(buf));
                 }
-                // printf("%s",buf,n);
-                sz = write(f2,buf,n);
-                if(end)break;
-            }   
-            close(f2);
-            close(fd);
+                write(fd,"\0",1);
 
-            // sending the encrypted file to the client
-            f2 = open("foo.txt.enc",O_RDWR);
-            n=-1;
-            while(n!=0)
-            {
-                n=read(f2,buf,100);
-                send(newsock,buf,n,0);
+                printf("File contents received and stored in %s\n",filename);
+                close(fd);
+
+                // now reading from the file ,ecrypting it and storing it enc.txt
+                fd = open(filename,O_RDWR);
+
+                char encfilename[1024];
+                sprintf(encfilename,"%s.%d.txt.enc",inet_ntoa(cliaddr.sin_addr),ntohs(cliaddr.sin_port));
+
+                int f2 = open(encfilename,O_RDWR|O_CREAT|O_TRUNC,0666);
+                if(f2<0)
+                {
+                    printf("unable to open %s\n",encfilename);
+                }
+
+
+                int n = -1;
+
+                end = 0;
+                while(1)
+                {
+                    n = read(fd,buf,100);
+                    if(n==0)break;
+                    for(int i=0;i<n;i++)
+                    {
+                        if(buf[i]=='#')
+                        {
+                            end = 1;
+                        }
+                        if(buf[i]>='a' && buf[i]<='z')
+                        {
+                            buf[i] = (buf[i]-'a'+k)%26 + 'a';
+                        }
+                        else if(buf[i]>='A' && buf[i]<='Z')
+                        {
+                            buf[i] = (buf[i]-'A'+k)%26 + 'A';
+                        }
+                    }
+                    // printf("%s",buf,n);
+                    sz = write(f2,buf,n);
+                    if(end)break;
+                }   
+                close(f2);
+                close(fd);
+
+                // sending the encrypted file to the client
+                f2 = open(encfilename,O_RDWR);
+                n=-1;
+                while(n!=0)
+                {
+                    n=read(f2,buf,100);
+                    send(newsock,buf,n,0);
+                }
+
+                printf("\nThe file has been encrypted and sent back to client\n");
             }
-
-            printf("\nThe file has been encrypted and sent back to client\n");
             close(newsock);
             exit(0);
         }
